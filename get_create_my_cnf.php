31ecce12eb70c788092149904505944d2b17e4ac
Taras Shkodenko <taras@shkodenko.com>
2017-07-24 12:56:08 +0300

    Refactored all scripts to use one CMS detect logic

--- get_create_my_cnf.php
+++ get_create_my_cnf.php
@@ -39,105 +39,14 @@ if (isset($argv[1])) {
 echo '# Checking directory: ' . $dirName . PHP_EOL;
 
 $disPlayResults = 0;
-$dbName = '';
-$dbUser = '';
-$dbPass = '';
-$dbHost = 'localhost';
-$configFile = '';
-$configFile = $dirName . DIRECTORY_SEPARATOR . 'wp-config.php';
-if (file_exists($configFile)) {
-    // WordPress CMS ?
-    echo '# Detected WordPress CMS' . PHP_EOL;
-    $fileLines = file($configFile);
-    $numConfigLines = count($fileLines);
-    if ($numConfigLines > 0) {
-        foreach ($fileLines as $line1) {
-            if (preg_match("/define\('DB_NAME', '(.+)'\);/m", $line1, $m1)) {
-                $dbName = $m1[1];
-            }
-            if (preg_match("/define\('DB_USER', '(.+)'\);/m", $line1, $m2)) {
-                $dbUser = $m2[1];
-            }
-            if (preg_match("/define\('DB_HOST', '(.+)'\);/m", $line1, $m3)) {
-                $dbHost = $m3[1];
-            }
-            if (preg_match("/define\('DB_PASSWORD', '(.+)'\);/m", $line1, $m3)) {
-                $dbPass = $m3[1];
-            }
-        }
-    }
-    if (strlen($dbName) > 0 && strlen($dbUser) > 0 && strlen($dbPass) > 0) {
-        $disPlayResults = 1;
-    }
-} else {
-    // Drupal ?
-    $configFile = $dirName . DIRECTORY_SEPARATOR . 'sites/default/settings.php';
-    if (file_exists($configFile)) {
-        include_once $configFile;
-        if (isset($db_url)) {
-            // Drupal 6 ?
-            echo '# Detected Drupal 6' . PHP_EOL;
-            $ap1 = parse_url($db_url);
-            // echo 'Parsed URL: ' . var_export($ap1, 1) . PHP_EOL;
-            $dbName = substr($ap1['path'], 1);
-            $dbUser = $ap1['user'];
-            $dbHost = $ap1['host'];
-            $dbPass = $ap1['pass'];
-            if (strlen($dbName) > 0 && strlen($dbUser) > 0 && strlen($dbPass) > 0) {
-                $disPlayResults = 1;
-            }
-        } else {
-            if (isset($databases)) {
-                // Drupal 7 ?
-                echo '# Detected Drupal 7' . PHP_EOL;
-                $dbName = $databases['default']['default']['database'];
-                $dbUser = $databases['default']['default']['username'];
-                $dbHost = $databases['default']['default']['host'];
-                $dbPass = $databases['default']['default']['password'];
-                if (strlen($dbName) > 0 
-                    && strlen($dbUser) > 0 
-                    && strlen($dbPass) > 0
-                ) {
-                    $disPlayResults = 1;    
-                }
-            }    
-        }    
-    }
-    // Joomla! CMS?
-    $configFile = $dirName . DIRECTORY_SEPARATOR . 'configuration.php';
-    if (file_exists($configFile)) {
-        include_once $configFile;
-        // Joomla!
-        echo '# Detected Joomla! CMS' . PHP_EOL;
-        $jc1 = new JConfig;
-        $dbHost = $jc1->host;
-        $dbUser = $jc1->user;
-        $dbPass = $jc1->password;
-        $dbName = $jc1->db;
-        if (strlen($dbName) > 0 && strlen($dbUser) > 0 && strlen($dbPass) > 0) {
-            $disPlayResults = 1;    
-        }
-    }
-    // Magento ?
-    $configFile = $dirName . DIRECTORY_SEPARATOR . 'app/etc/local.xml';
-    if (file_exists($configFile)) {
-        if (!function_exists('simplexml_load_file')) {
-            echo 'Error: can`t work without simplexml_load_file function.' .
-                ' Please install SimpleExml support in PHP.' . PHP_EOL;
-            exit;
-        }
-        $xmlConfig = simplexml_load_file($configFile);
-        echo '# Detected Magento' . PHP_EOL;
-        $conn = $xmlConfig->global->resources->default_setup->connection;
-        $dbHost = (string) $conn->host;
-        $dbUser = (string) $conn->username;
-        $dbPass = (string) $conn->password;
-        $dbName = (string) $conn->dbname;
-        if (strlen($dbName) > 0 && strlen($dbUser) > 0 && strlen($dbPass) > 0) {
-            $disPlayResults = 1;    
-        }
-    }
-}
+$dbs[0] = array(
+    'host' => 'localhost',
+    'name' => '',
+    'user' => '',
+    'pass' => '',
+);
+
+require_once 'inc' . DIRECTORY_SEPARATOR . 'detect_cms.php';
 
 $msgTemplate =<<<EOM
 #
